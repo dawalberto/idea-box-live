7704a5adb09bca6c6a9f4fa87b0dc9b5cfbf09b0
dawalberto <albertoinformatica97@gmail.com>
2021-08-20 17:46:19 +0200

    agregado componente AppIdea

--- src/App.vue
+++ src/App.vue
@@ -14,29 +14,23 @@
     </section>
 
     <!-- Idea item -->
-    <article class="p-3 sm:flex sm:items-center rounded-lg bg-gray-300">
-      <!-- Info -->
-      <section class="text-center sm:flex-1 sm:text-left">
-        <h2 class="text-xl sm:leading-6 sm:text-2xl">This is an idea title and must be collapsed on another row</h2>
-        <small>Username</small>
-      </section>
-      <!-- Vote -->
-      <section class="pt-3 border-t-2 mt-6 border-black sm:pt-0 sm:pl-3 sm:border-t-0 sm:border-l-2 sm:mt-0 sm:flex sm:items-center">
-        <h3 class="text-3xl font-bold text-center">99</h3>
-        <nav class="flex justify-center sm:block">
-          <img class="w-10 cursor-pointer" src="@/assets/img/arrow.svg" alt="Vote up">
-          <img class="w-10 cursor-pointer transform rotate-180" src="@/assets/img/arrow.svg" alt="Vote down">
-        </nav>
-      </section>
-    </article>
+    <AppIdea v-for="(idea, $index) of ideas" :key="$index" :idea="idea" />
   </div>
 </div>
 </template>
 
 <script>
+import AppIdea from "@/components/AppIdea.vue"
+import seed from "@/seed.json"
+import { ref } from "vue"
 
 export default {
   name: "App",
+  components: { AppIdea },
+  setup() {
+    const ideas = ref(seed.ideas)
+    return { ideas }
+  }
 };
 </script>
 
